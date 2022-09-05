ac7f327c0db8bc0535a40de90642cab6e1419052
hareshbaskaran <baskaranharesh@gmail.com>
2022-09-05 15:12:27 +0530

    All Ui fixed - before cat work done

--- mic_event_notifier/lib/HomeScreen.dart
+++ mic_event_notifier/lib/HomeScreen.dart
@@ -1,10 +1,10 @@
-import 'dart:ui';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'main.dart';
 import 'loginpage.dart';
-int notification_bool=0;
+String event_title ='open mic';
+bool notification_bool = false;
 int slider_event = eventname.length;
 final List<String> imgList = [
   'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
@@ -94,17 +94,12 @@ class _HomeScreenState extends State<HomeScreen> {
                                   ),
                                   style: ElevatedButton.styleFrom(
                                       backgroundColor:
-                                      (notification_bool==1)?
-                                          bfont:
                                       Color(0xffF0F0F0),
                                       shape: CircleBorder()/*const RoundedRectangleBorder(
                                         borderRadius:
                                         BorderRadius.all(Radius.circular(180))),*/
                                   ),
                                   onPressed: () {
-                                    setState(() {
-                                      notification_bool==1;
-                                    });
                                   },
                                 ),
                               ),
@@ -199,6 +194,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           )
                         ],
                       ),
+                      EventCard()
                     ]
                 ),
               ),
@@ -207,71 +203,116 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
+class EventCard extends StatefulWidget {
+  const EventCard({Key? key}) : super(key: key);
 
-///todo:design this card according to UI - for Shalini
-  Widget  EventCard() {
-    return Card(
-      child: CustomScrollView(
-        slivers: <Widget>[
-          SliverList(
-              delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index){
-                    return Container(
-                      width: 300,
-                      height: 200,
-                      padding: new EdgeInsets.all(10.0),
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0)
-                        ),
-                        color: vhome,
-                        elevation: 15,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            const ListTile(
-                              title: Text(
-                                'Open Mic',
-                                style: TextStyle(fontSize: 30.0),
-                              ),
-                              subtitle: Text(
-                                'MICROSOFT INNVATION CLUB',
-                                style: TextStyle(fontSize: 18.0),
-                              ),
-                              trailing: Icon(Icons.check_circle),
-                            ),
-                            Divider(
-                              color: Colors.black,
-                              height: 25,
-                              thickness: 2,
-                              indent: 5,
-                              endIndent: 5,
-                            ),
-                            const ListTile(
-                              leading: Icon(Icons.calendar_today_rounded),
-                              title: Text(
-                                  '11:30 AM - 12:30 PM',
-                                style: TextStyle(fontSize: 18.0),
-                              ),
-                              subtitle: Text(
-                                'Mon, 17 Sept 2022',
-                                style: TextStyle(fontSize: 18.0),
-                              ),
-                              trailing: Icon(Icons.plus_one),
-                            ),
-                          ],
-                        ),
+  @override
+  State<EventCard> createState() => _EventCardState();
+}
+
+class _EventCardState extends State<EventCard> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Expanded(
+        child: Container(
+          width: MediaQuery.of(context).size.width*0.9,
+          padding: new EdgeInsets.all(10.0),
+          child: Card(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0)
+            ),
+            color: Colors.white,
+            elevation: 15,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ListTile(
+                  title: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Open Mic',
+                      style: GoogleFonts.workSans(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 32,
                       ),
-                    );
-                  }
-              ),
+                    ),
+                  ),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'MICROSOFT INNVATION CLUB',
+                      style: GoogleFonts.workSans(
+                        color: bdiv,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  trailing: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 20,
+                        bottom: 20
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          (notification_bool==false)?
+                          notification_bool=true:
+                              notification_bool=false;
+
+                        });
+                      },
+                      icon:Icon(
+                          Icons.check_circle,
+                          color: (notification_bool==true)?
+                              Colors.blueAccent:bdiv
+                      ),
+                    ),
+                  ),
+                ),
+                Divider(
+                  color: bdiv,
+                  height: 15,
+                  thickness: 0.5,
+                  indent: 20,
+                  endIndent: 20,
+                ),
+                ListTile(
+                  leading: Icon(Icons.calendar_today_rounded,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                  title: Text(
+                    '11:30 AM - 12:30 PM',
+                    style: GoogleFonts.workSans(
+                      color: bdiv,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14,
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Mon, 17 Sept 2022',
+                    style: GoogleFonts.workSans(
+                      color: bdiv,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
+}
 
-
+///todo:design this card according to UI - for Shalini
 
 final List<Widget>
 imageSliders =
