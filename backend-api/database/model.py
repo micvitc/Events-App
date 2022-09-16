0dacd7107df02211c78fd357ba7bdf95635adbc9
Tulasi Raman <85024126+ItsTulasiRaman@users.noreply.github.com>
2022-09-16 14:06:43 +0530

    Updated ambiguity in arguments.

--- backend-api/database/model.py
+++ backend-api/database/model.py
@@ -1,12 +1,12 @@
 from .db import db
 
 class Event(db.Document):
-    event_id = db.StringField(required=True, unique=True)
+    event_id = db.StringField(primary_key=True)
     event_name = db.StringField(required=True)
     event_date = db.DateTimeField(required=True)
     event_venue = db.StringField(required = True)
     event_host = db.StringField(required=True)
     event_coordinators = db.StringField()
-    user_id = db.IntField(primary_key=True, unique=True)
+    user_id = db.IntField(primary_key=True)
     user_name = db.StringField()
     
