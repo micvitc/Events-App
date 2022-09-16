8a3266f2d03f010669f48a051ad3f0f002941103
Tulasi Raman <85024126+ItsTulasiRaman@users.noreply.github.com>
2022-09-16 14:00:37 +0530

    Added some additional variables in ORM.

--- backend-api/database/model.py
+++ backend-api/database/model.py
@@ -4,5 +4,9 @@ class Event(db.Document):
     event_id = db.StringField(required=True, unique=True)
     event_name = db.StringField(required=True)
     event_date = db.DateTimeField(required=True)
-    event_venue = db.StringField(required=True)
+    event_venue = db.StringField(required = True)
+    event_host = db.StringField(required=True)
+    event_coordinators = db.StringField()
+    user_id = db.IntField(primary_key=True, unique=True)
+    user_name = db.StringField(required=True)
     
