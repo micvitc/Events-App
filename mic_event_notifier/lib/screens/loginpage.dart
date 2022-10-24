8c43c469ea3c243011562765e380d670ab4a59d6
hareshbaskaran <baskaranharesh@gmail.com>
2022-10-24 13:33:34 +0530

    already login - sign in button check

--- mic_event_notifier/lib/screens/loginpage.dart
+++ mic_event_notifier/lib/screens/loginpage.dart
@@ -81,57 +81,7 @@ class _MyLoginState extends State<MyLogin> {
                 ),
               ),
             ),
-            Padding(
-            padding: const EdgeInsets.only(bottom: 50),
-            child: OutlinedButton(
-            style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(Colors.white),
-            shape: MaterialStateProperty.all(
-            RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(40),
-            ),
-            ),
-            ),
-            onPressed: () async {
-            User? user =
-            await Authentication.signInWithGoogle(context: context);
-
-            if (user != null) {
-            Navigator.of(context).pushReplacement(
-            MaterialPageRoute(
-            builder: (context) => HomeScreen(
-            user: user,
-            ),
-            ),
-            );
-            }
-            },
-            child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-    child: Row(
-    mainAxisSize: MainAxisSize.min,
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-    Image(
-    image: AssetImage("assets/google_logo.png"),
-    height: 35.0,
-    ),
-    Padding(
-    padding: const EdgeInsets.only(left: 10),
-    child: Text(
-    'Sign in with Google',
-    style: TextStyle(
-    fontSize: 20,
-    color: Colors.black54,
-    fontWeight: FontWeight.w600,
-    ),
-    ),
-    )
-    ],
-    ),
-    ),
-    ),
-    )
+            GoogleSignInButton()
           ],
         ),
       ),
