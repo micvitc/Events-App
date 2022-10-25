3818f7e673dc3974ac834851373b78d4a48d419d
Rishabh Kumar Sharma <rishabhsharma28302@gmail.com>
2022-10-25 16:43:16 +0530

    added toast

--- mic_event_notifier/lib/widgets/google_sign_in_button.dart
+++ mic_event_notifier/lib/widgets/google_sign_in_button.dart
@@ -2,7 +2,10 @@ import 'package:Login_ui/screens/HomeScreen.dart';
 import 'package:Login_ui/utils/authentication.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 int loginerror = 0;
+
 class GoogleSignInButton extends StatefulWidget {
   @override
   _GoogleSignInButtonState createState() => _GoogleSignInButtonState();
@@ -14,65 +17,72 @@ class _GoogleSignInButtonState extends State<GoogleSignInButton> {
     return Padding(
       padding: const EdgeInsets.all(20),
       child: OutlinedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.white),
-                shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.white),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40),
+            ),
+          ),
+        ),
+        onPressed: () async {
+          User? user = await Authentication.signInWithGoogle(context: context);
+          bool vitcheck = user!.email!.contains('vitstudent.ac.in');
+          if (user != null && vitcheck == true) {
+            {
+              setState(() {
+                loginerror = 0;
+              });
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (context) => HomeScreen(
+                    user: user,
                   ),
                 ),
+              );
+            }
+          }
+          if (vitcheck == false) {
+            Authentication.signOut(context: context);
+            setState(() {
+              loginerror = 1;
+            });
+            Fluttertoast.showToast(
+                msg: "Invalid authentication",
+                toastLength: Toast.LENGTH_SHORT,
+                gravity: ToastGravity.CENTER,
+                timeInSecForIosWeb: 1,
+                backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                textColor: Color.fromARGB(255, 0, 0, 0),
+                fontSize: 16.0);
+            print("no firebase initialised");
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                image: AssetImage("assets/google_logo.png"),
+                height: 35.0,
               ),
-              onPressed: () async {
-                User? user = await Authentication.signInWithGoogle(context: context);
-                bool vitcheck = user!.email!.contains('vitstudent.ac.in');
-                if (user != null&& vitcheck==true) {
-                   {
-                     setState(() {
-                       loginerror=0;
-                     });
-                    Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(
-                      builder: (context) => HomeScreen(
-                        user: user,
-                      ),
-                    ),
-                  );
-                  }
-                }
-                if(vitcheck==false){
-                  Authentication.signOut(context: context);
-                  setState(() {
-                    loginerror=1;
-                  });
-                  print(loginerror);
-                  print("no firebase initialised");
-                }
-              },
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Image(
-                      image: AssetImage("assets/google_logo.png"),
-                      height: 35.0,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Text(
-                        'Sign in with Google',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black54,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    )
-                  ],
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(
+                  'Sign in with Google',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-            ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
