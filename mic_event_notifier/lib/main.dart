093dfa9ddf914474fb0d404a01e04571d653f7bd
Master Sri Kavipriyan <68172796+MasterKavi-24@users.noreply.github.com>
2022-10-26 20:27:05 +0530

    Update main.dart

--- mic_event_notifier/lib/main.dart
+++ mic_event_notifier/lib/main.dart
@@ -1,8 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:Login_ui/screens/loginpage.dart';
+import 'package:Login_ui/services/notif_services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
 
 //DE7BDE
 Color vwhite = Color(0xffF3F3F3);
@@ -13,6 +17,8 @@ Color r = Color(0xffDB4437);
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await NotificationService().initNotification();
+  tz.initializeTimeZones();
   runApp(MyApp());
 }
 
