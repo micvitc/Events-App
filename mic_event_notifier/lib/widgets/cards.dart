f290b13d761c3f61dbd0046e0b39b6ee8ca989de
MasterKavi-24 <srikavipriyan3@gmail.com>
2022-10-27 18:52:56 +0530

    Update cards.dart

--- mic_event_notifier/lib/widgets/cards.dart
+++ mic_event_notifier/lib/widgets/cards.dart
@@ -90,9 +90,9 @@ class _CardsState extends State<Cards> {
             diff-(60*60*24),
           );
         }
-        // else {
-          // print("Event is 1 day too close to be reminded");
-        // }
+        else {
+          print("Event is 1 day too close to be reminded");
+        }
 
         if (diff-(60*60) > 0) {
           NotificationService().showNotification(
@@ -102,9 +102,9 @@ class _CardsState extends State<Cards> {
             diff-(60*60),
           );
         }
-        // else {
-          // print("Event is 1 hour too close to be reminded");
-        // }
+        else {
+          print("Event is 1 hour too close to be reminded");
+        }
 
         if (diff-(60*15) > 0) {
           NotificationService().showNotification(
@@ -114,31 +114,32 @@ class _CardsState extends State<Cards> {
             diff-(60*15),
           );
         }
-        // else {
-          // print("Event is 15 minutes too close to be reminded");
-        // }
+        else {
+          print("Event is 15 minutes too close to be reminded");
+        }
         
-        // if (diff > 0) {
-        //   NotificationService().showNotification(
-        //     int.parse(event.eventId.toString()),
-        //     event.eventName.toString(),
-        //     'The event will start by ${dater.dayTime}', // body,
-        //     diff,
-        //   );
-        // }
-        // else {
-        //   // print("Event is 0 seconds too close to be reminded");
-        //   NotificationService().showNotification(
-        //     int.parse(event.eventId.toString()),
-        //     event.eventName.toString(),
-        //     'The event has started', // body,
-        //     1,
-        //   );
-        // }
+        if (diff > 0) {
+          NotificationService().showNotification(
+            int.parse(event.eventId.toString()),
+            event.eventName.toString(),
+            'The event will start by ${dater.dayTime}', // body,
+            diff,
+          );
+        }
+        else {
+          // print("Event is 0 seconds too close to be reminded");
+          NotificationService().showNotification(
+            int.parse(event.eventId.toString()),
+            event.eventName.toString(),
+            'The event has started', // body,
+            1,
+          );
+        }
+        
       }
-      else {
-        NotificationService().cancelNotification(int.parse(event.eventId.toString()));
-      }
+      // else {
+      //   NotificationService().cancelNotification(int.parse(event.eventId.toString()));
+      // }
       notif[index] = !notif[index];
     });
   }
@@ -238,6 +239,453 @@ class _CardsState extends State<Cards> {
   }
 }
 
+
+// // ignore_for_file: prefer_const_constructors
+
+// import 'package:Login_ui/models/EventModel.dart';
+// import 'package:Login_ui/screens/HomeScreen.dart';
+// import 'package:Login_ui/main.dart';
+// import 'package:Login_ui/services/api_services.dart';
+// import 'package:Login_ui/services/notif_services.dart';
+// import 'package:Login_ui/services/shared_prefs.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'dart:convert';
+// import 'package:Login_ui/services/dater.dart';
+
+// import 'package:flutter/services.dart' as root_bundle;
+
+// int count = 0;
+
+// // // Fetching Json file.
+// // Future<List<EventModel>> ReadJsonData() async {
+// //   eventList = [];
+// //   final jsonresponse =
+// //       await root_bundle.rootBundle.loadString('jsonfile/testing-api.json');
+// //   final data = await json.decode(jsonresponse);
+// //   for (Map i in data) {
+// //     count += 1;
+// //     eventList.add(EventModel.fromJson(i));
+// //   }
+// //   return eventList;
+// // }
+
+// List<bool> notif = List.filled(0, false, growable: true);
+// // List<bool> notif = [];
+
+// // ignore: non_constant_identifier_names
+// Future<List<EventModel>> ReadJsonData() async {
+//   List<EventModel> eventList_ = await APIServices.getEvents();
+//   // for (EventModel event in value) {
+//   //       if ((SharedPrefs.read(event.eventName.toString()) == "true")) {
+//   //         notif.add(true);
+//   //       } else {
+//   //         SharedPrefs.save(event.eventName.toString(), "false");
+//   //         notif.add(false);
+//   //       }
+//   //     }
+//   count = 0;
+//   // print(notif);
+//   // if (!(notif == [false])) {
+//     // notif = [];
+//     for (EventModel event in eventList_) {
+
+//       // String temp = await SharedPrefs.read(event.eventName.toString());
+//       // // print("${event.eventName} $temp");
+//       // // if (temp == "true") {
+//       // //   print("true pref");
+//       // //   notif.add(true);
+//       // // }
+//       // // else if(temp == "") {
+//       // //   print("empty pref");
+//       // //   SharedPrefs.save(event.eventName.toString(), "true");
+//       // //   notif.add(true);
+//       // // }
+//       // // else {
+//       // //   print("false pref");
+//       // //   SharedPrefs.save(event.eventName.toString(), "false");
+//       // //   notif.add(false);
+//       // // }
+
+//       count += 1;
+//     }
+//   // }
+//   // print(count);
+//   return eventList_;
+// }
+
+// // Future<int> getLen() async {
+// //   List<EventModel> eventList = await APIServices.getEvents();
+// //   count = 0;
+// //   for (var i in eventList) {
+// //     count += 1;
+// //   }
+// //   return count;
+// // }
+
+// class EventsCard extends StatefulWidget {
+//   const EventsCard({super.key});
+
+//   @override
+//   State<EventsCard> createState() => _EventsCardState();
+// }
+
+// class _EventsCardState extends State<EventsCard> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return SingleChildScrollView(
+//       child: Container(
+//           width: MediaQuery.of(context).size.width * 0.9,
+//           height: MediaQuery.of(context).size.width,
+//           padding: new EdgeInsets.all(10.0),
+//           child: Cards()),
+//     );
+//   }
+// }
+
+// class Cards extends StatefulWidget {
+//   const Cards({super.key});
+
+//   @override
+//   State<Cards> createState() => _CardsState();
+// }
+
+// class _CardsState extends State<Cards> {
+
+  
+//   @override
+//   void initState() {
+
+//     // Future<int> count = getLen();
+//     // ReadJsonData();
+
+//     Future<List<EventModel>> eventL = ReadJsonData();
+//     eventL.then((value) => () {
+//       for (EventModel event in value) {
+//         if ((SharedPrefs.read(event.eventName.toString()) == "true")) {
+//           notif.add(true);
+//         } else {
+//           SharedPrefs.save(event.eventName.toString(), "false");
+//           notif.add(false);
+//         }
+//       }
+//     });
+
+//     super.initState();
+
+//   }
+
+//   void setNotif(EventModel event, int index) {
+//     setState(() {
+//       if (notif[index] == false) {
+//         notif[index] = true;
+//         Dater dater = Dater(millis: int.parse(event.eventDate.toString()));
+
+//         // print(dater.dateSeconds);
+//         // print(dater.dayTime);
+//         // print(dater.nowSeconds);
+//         // print(dater.nowDayTime);
+
+//         // int diff = ((dater.difference)/60).round();
+//         int diff = dater.difference;
+
+//         // print(diff);
+//         // print(dater.secondsToTime(diff));
+
+//         if (diff-(60*60*24) > 0) {
+//           NotificationService().showNotification(
+//             int.parse(event.eventId.toString()),
+//             event.eventName.toString(),
+//             '1 day reminder for ${event.eventName}', // body,
+//             diff-(60*60*24),
+//           );
+//         }
+//         // else {
+//         //   print("Event is 1 day too close to be reminded");
+//         // }
+
+//         if (diff-(60*60) > 0) {
+//           NotificationService().showNotification(
+//             int.parse(event.eventId.toString()),
+//             event.eventName.toString(),
+//             '1 hour reminder for ${event.eventName}', // body,
+//             diff-(60*60),
+//           );
+//         }
+//         // else {
+//         //   print("Event is 1 hour too close to be reminded");
+//         // }
+
+//         if (diff-(60*15) > 0) {
+//           NotificationService().showNotification(
+//             int.parse(event.eventId.toString()),
+//             event.eventName.toString(),
+//             '15 minute reminder for ${event.eventName}', // body,
+//             diff-(60*15),
+//           );
+//         }
+//         // else {
+//         //   print("Event is 15 minutes too close to be reminded");
+//         // }
+        
+//         if (diff > 0) {
+//           NotificationService().showNotification(
+//             int.parse(event.eventId.toString()),
+//             event.eventName.toString(),
+//             'The event will start by ${dater.dayTime}', // body,
+//             diff,
+//           );
+//         }
+//         else {
+//           // print("Event is 0 seconds too close to be reminded");
+//           NotificationService().showNotification(
+//             int.parse(event.eventId.toString()),
+//             event.eventName.toString(),
+//             'The event has started', // body,
+//             1,
+//           );
+//         }
+//       }
+      
+//       // else {
+//       //   notif[index] = false;
+//       //   NotificationService().cancelNotification(int.parse(event.eventId.toString()));
+//       // }
+
+//       print(notif);
+//       notif[index] = !notif[index];
+      
+//     });
+//   }
+
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder(
+//         // future: APIServices.getEvents(),
+//         future: ReadJsonData(),
+//         builder: (context, AsyncSnapshot<List<EventModel>> snapshot) {
+//           print(count);
+//           if (!snapshot.hasData) {
+//             return CircularProgressIndicator();
+//           } else {
+//             return ListView.builder(
+//                 itemCount: count,
+//                 itemBuilder: (context, index) {
+//                   print(snapshot.data.toString() + " " + count.toString() + " " + index.toString() + " " + notif.toString());
+//                   return Card(
+//                     shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(15.0)),
+//                     color: Colors.white,
+//                     elevation: 15,
+//                     child: Column(children: <Widget>[
+//                       ListTile(
+//                         title: Padding(
+//                           padding: const EdgeInsets.all(8.0),
+//                           child: Text(
+//                             snapshot.data![index].eventName.toString(),
+//                             style: GoogleFonts.workSans(
+//                               color: Colors.black,
+//                               fontWeight: FontWeight.w600,
+//                               fontSize: 25,
+//                             ),
+//                           ),
+//                         ),
+//                         subtitle: Padding(
+//                           padding: const EdgeInsets.all(8.0),
+//                           child: Text(
+//                             snapshot.data![index].eventHost.toString(),
+//                             style: GoogleFonts.workSans(
+//                               color: bdiv,
+//                               fontWeight: FontWeight.w400,
+//                               fontSize: 16,
+//                             ),
+//                           ),
+//                         ),
+//                         trailing: Padding(
+//                           padding: const EdgeInsets.only(left: 20, top: 10),
+//                           child: IconButton(
+//                             onPressed: () {
+//                               setNotif(snapshot.data![index], index);
+//                               // String idd = snapshot.data![index].eventId.toString();
+//                               // setNotif(int.parse(idd));
+//                             },
+//                             icon: Icon(Icons.check_circle,
+//                                 size: 35,
+//                                 color: (notif[index] == true) ? Colors.blueAccent : bdiv),
+//                           ),
+//                         ),
+//                       ),
+//                       Divider(
+//                         color: bdiv,
+//                         height: 10,
+//                         thickness: 0.5,
+//                         indent: 20,
+//                         endIndent: 20,
+//                       ),
+//                       ListTile(
+//                         title: Padding(
+//                           padding: const EdgeInsets.fromLTRB(0.0, 0.0, 120.0, 0.0),
+//                           child: Column(children: <Widget>[
+//                             Text(
+//                               Dater(millis: int.parse(snapshot.data![index].eventDate.toString())).dayTime,
+//                               textAlign: TextAlign.left,
+//                               style: GoogleFonts.workSans(
+//                                 color: bdiv,
+//                                 fontWeight: FontWeight.w400,
+//                                 fontSize: 15,
+//                               ),
+//                             ),
+//                           ]),
+//                         ),
+//                         leading:Padding(
+//                           padding: const EdgeInsets.only(bottom: 6.0),
+//                           //child: Icon(Icons.calendar_today_outlined),
+//                           child: IconButton(
+//                             onPressed: () {
+//                             },
+//                             icon: Icon(Icons.calendar_today_outlined,),
+//                           ),
+//                         ),
+//                       ),
+//                     ]),
+//                   );
+//                 });
+//           }
+//         });
+//   }
+// }
+
+
+// // ignore_for_file: prefer_const_constructors
+
+// import 'package:Login_ui/models/EventModel.dart';
+// import 'package:Login_ui/screens/HomeScreen.dart';
+// import 'package:Login_ui/main.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'dart:convert';
+
+// import 'package:flutter/services.dart' as root_bundle;
+
+// List<EventModel> eventList = [];
+// // Fetching Json file.
+// Future<List<EventModel>> ReadJsonData() async {
+//   eventList = [];
+//   final jsonresponse =
+//       await root_bundle.rootBundle.loadString('jsonfile/testing-api.json');
+//   final data = await json.decode(jsonresponse);
+//   for (Map i in data) {
+//     eventList.add(EventModel.fromJson(i));
+//   }
+//   return eventList;
+// }
+
+// class EventsCard extends StatefulWidget {
+//   const EventsCard({super.key});
+
+//   @override
+//   State<EventsCard> createState() => _EventsCardState();
+// }
+
+// class _EventsCardState extends State<EventsCard> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return SingleChildScrollView(
+//       child: Container(
+//           width: MediaQuery.of(context).size.width * 0.9,
+//           height: MediaQuery.of(context).size.width,
+//           padding: new EdgeInsets.all(8.0),
+//           child: Cards()),
+//     );
+//   }
+// }
+
+// class Cards extends StatefulWidget {
+//   const Cards({super.key});
+
+//   @override
+//   State<Cards> createState() => _CardsState();
+// }
+
+// class _CardsState extends State<Cards> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder(
+//         future: ReadJsonData(),
+//         builder: (context, AsyncSnapshot<List<EventModel>> snapshot) {
+//           if (!snapshot.hasData) {
+//             return CircularProgressIndicator();
+//           } else {
+//             return ListView.builder(
+//                 itemCount: eventList.length,
+//                 itemBuilder: (context, index) {
+//                   return Card(
+//                     shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(15.0)),
+//                     color: Colors.white,
+//                     elevation: 15,
+//                     child: Column(children: <Widget>[
+//                       ListTile(
+//                         title: Padding(
+//                           padding: const EdgeInsets.all(8.0),
+//                           child: Column(children: <Widget>[
+//                             Text(
+//                               snapshot.data![index].eventName.toString(),
+//                               style: GoogleFonts.workSans(
+//                                 color: Colors.black,
+//                                 fontWeight: FontWeight.w600,
+//                                 fontSize: 22,
+//                               ),
+//                             ),
+//                             Divider(
+//                               color: bdiv,
+//                               height: 10,
+//                               thickness: 0.5,
+//                               indent: 20,
+//                               endIndent: 20,
+//                             ),
+//                           ]),
+//                         ),
+//                         subtitle: Padding(
+//                           padding: const EdgeInsets.all(8.0),
+//                           child: Text(
+//                             snapshot.data![index].eventHost.toString(),
+//                             style: GoogleFonts.workSans(
+//                               color: bdiv,
+//                               fontWeight: FontWeight.w400,
+//                               fontSize: 16,
+//                             ),
+//                           ),
+//                         ),
+//                         trailing: Padding(
+//                           padding: const EdgeInsets.only(left: 20, bottom: 20),
+//                           child: IconButton(
+//                             onPressed: () {
+//                               setState(() {
+//                                 (notification_bool == false)
+//                                     ? notification_bool = true
+//                                     : notification_bool = false;
+//                               });
+//                             },
+//                             icon: Icon(Icons.check_circle,
+//                                 color: (notification_bool == true)
+//                                     ? Colors.blueAccent
+//                                     : bdiv),
+//                           ),
+//                         ),
+//                       ),
+//                     ]),
+//                   );
+//                 });
+//           }
+//         });
+//   }
+// }
+
+
+
+///////////////////////////
 
 // // ignore_for_file: prefer_const_constructors
 
